bc06bd1e9df9bd2b15cbba074390ea3e689075da
Agayev Pervin Nadir <Agaye_jz58@itstep.edu.az>
2023-10-10 12:21:49 +0400

    some

--- calculator/Form1.Designer.cs
+++ calculator/Form1.Designer.cs
@@ -44,20 +44,19 @@
             button14 = new Button();
             button15 = new Button();
             button16 = new Button();
-            panel1 = new Panel();
             lbl_result = new Label();
             tboxscreen = new RichTextBox();
             label1 = new Label();
-            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // button1
             // 
             button1.BackColor = Color.Lime;
             button1.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button1.Location = new Point(77, 330);
+            button1.Location = new Point(82, 325);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(63, 65);
+            button1.Size = new Size(55, 49);
             button1.TabIndex = 0;
             button1.Text = "X";
             button1.UseVisualStyleBackColor = false;
@@ -67,9 +66,10 @@
             // 
             button2.BackColor = Color.Lime;
             button2.Font = new Font("Showcard Gothic", 24F, FontStyle.Bold, GraphicsUnit.Point);
-            button2.Location = new Point(213, 331);
+            button2.Location = new Point(219, 325);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(58, 64);
+            button2.Size = new Size(51, 48);
             button2.TabIndex = 1;
             button2.Text = "-";
             button2.TextAlign = ContentAlignment.TopCenter;
@@ -80,9 +80,10 @@
             // 
             button3.BackColor = Color.Lime;
             button3.Font = new Font("Showcard Gothic", 22.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button3.Location = new Point(163, 330);
+            button3.Location = new Point(176, 325);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(55, 65);
+            button3.Size = new Size(48, 49);
             button3.TabIndex = 2;
             button3.Text = "+";
             button3.UseVisualStyleBackColor = false;
@@ -92,9 +93,10 @@
             // 
             button4.BackColor = Color.Lime;
             button4.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button4.Location = new Point(125, 330);
+            button4.Location = new Point(130, 324);
+            button4.Margin = new Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new Size(46, 65);
+            button4.Size = new Size(56, 49);
             button4.TabIndex = 3;
             button4.Text = "/";
             button4.UseVisualStyleBackColor = false;
@@ -104,9 +106,10 @@
             // 
             button11.BackColor = Color.Crimson;
             button11.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button11.Location = new Point(201, 399);
+            button11.Location = new Point(209, 392);
+            button11.Margin = new Padding(3, 2, 3, 2);
             button11.Name = "button11";
-            button11.Size = new Size(70, 47);
+            button11.Size = new Size(61, 35);
             button11.TabIndex = 10;
             button11.Text = "3";
             button11.UseVisualStyleBackColor = false;
@@ -116,9 +119,10 @@
             // 
             button12.BackColor = Color.Crimson;
             button12.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button12.Location = new Point(135, 399);
+            button12.Location = new Point(143, 392);
+            button12.Margin = new Padding(3, 2, 3, 2);
             button12.Name = "button12";
-            button12.Size = new Size(69, 47);
+            button12.Size = new Size(60, 35);
             button12.TabIndex = 11;
             button12.Text = "2";
             button12.UseVisualStyleBackColor = false;
@@ -128,9 +132,10 @@
             // 
             button13.BackColor = Color.Crimson;
             button13.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button13.Location = new Point(77, 399);
+            button13.Location = new Point(82, 392);
+            button13.Margin = new Padding(3, 2, 3, 2);
             button13.Name = "button13";
-            button13.Size = new Size(63, 47);
+            button13.Size = new Size(55, 35);
             button13.TabIndex = 12;
             button13.Text = "1";
             button13.UseVisualStyleBackColor = false;
@@ -140,9 +145,10 @@
             // 
             button5.BackColor = Color.Crimson;
             button5.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button5.Location = new Point(201, 442);
+            button5.Location = new Point(204, 441);
+            button5.Margin = new Padding(3, 2, 3, 2);
             button5.Name = "button5";
-            button5.Size = new Size(70, 47);
+            button5.Size = new Size(61, 35);
             button5.TabIndex = 13;
             button5.Text = "6";
             button5.UseVisualStyleBackColor = false;
@@ -152,9 +158,10 @@
             // 
             button6.BackColor = Color.Crimson;
             button6.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button6.Location = new Point(141, 442);
+            button6.Location = new Point(143, 441);
+            button6.Margin = new Padding(3, 2, 3, 2);
             button6.Name = "button6";
-            button6.Size = new Size(63, 47);
+            button6.Size = new Size(55, 35);
             button6.TabIndex = 14;
             button6.Text = "5";
             button6.UseVisualStyleBackColor = false;
@@ -164,9 +171,10 @@
             // 
             button7.BackColor = Color.Crimson;
             button7.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button7.Location = new Point(77, 442);
+            button7.Location = new Point(82, 441);
+            button7.Margin = new Padding(3, 2, 3, 2);
             button7.Name = "button7";
-            button7.Size = new Size(63, 47);
+            button7.Size = new Size(55, 35);
             button7.TabIndex = 15;
             button7.Text = "4";
             button7.UseVisualStyleBackColor = false;
@@ -176,9 +184,10 @@
             // 
             button8.BackColor = Color.Crimson;
             button8.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button8.Location = new Point(139, 485);
+            button8.Location = new Point(143, 490);
+            button8.Margin = new Padding(3, 2, 3, 2);
             button8.Name = "button8";
-            button8.Size = new Size(63, 47);
+            button8.Size = new Size(55, 35);
             button8.TabIndex = 16;
             button8.Text = "8";
             button8.UseVisualStyleBackColor = false;
@@ -188,9 +197,10 @@
             // 
             button9.BackColor = Color.Crimson;
             button9.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button9.Location = new Point(77, 485);
+            button9.Location = new Point(82, 490);
+            button9.Margin = new Padding(3, 2, 3, 2);
             button9.Name = "button9";
-            button9.Size = new Size(63, 47);
+            button9.Size = new Size(55, 35);
             button9.TabIndex = 17;
             button9.Text = "7";
             button9.UseVisualStyleBackColor = false;
@@ -200,9 +210,10 @@
             // 
             button10.BackColor = Color.Crimson;
             button10.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button10.Location = new Point(201, 485);
+            button10.Location = new Point(204, 490);
+            button10.Margin = new Padding(3, 2, 3, 2);
             button10.Name = "button10";
-            button10.Size = new Size(70, 47);
+            button10.Size = new Size(61, 35);
             button10.TabIndex = 18;
             button10.Text = "9";
             button10.UseVisualStyleBackColor = false;
@@ -212,9 +223,10 @@
             // 
             button14.BackColor = Color.Crimson;
             button14.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button14.Location = new Point(139, 529);
+            button14.Location = new Point(143, 538);
+            button14.Margin = new Padding(3, 2, 3, 2);
             button14.Name = "button14";
-            button14.Size = new Size(63, 47);
+            button14.Size = new Size(55, 35);
             button14.TabIndex = 19;
             button14.Text = "0";
             button14.UseVisualStyleBackColor = false;
@@ -225,9 +237,10 @@
             button15.BackColor = Color.Crimson;
             button15.Cursor = Cursors.No;
             button15.Font = new Font("Showcard Gothic", 16.2F, FontStyle.Bold, GraphicsUnit.Point);
-            button15.Location = new Point(201, 529);
+            button15.Location = new Point(204, 538);
+            button15.Margin = new Padding(3, 2, 3, 2);
             button15.Name = "button15";
-            button15.Size = new Size(70, 47);
+            button15.Size = new Size(61, 35);
             button15.TabIndex = 20;
             button15.Text = ".";
             button15.UseVisualStyleBackColor = false;
@@ -239,44 +252,34 @@
             button16.BackgroundImage = Properties.Resources.png_transparent_computer_icons_history_symbol_previous_button_miscellaneous_angle_text_thumbnail;
             button16.BackgroundImageLayout = ImageLayout.Stretch;
             button16.Font = new Font("Showcard Gothic", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            button16.Location = new Point(77, 529);
+            button16.Location = new Point(82, 538);
+            button16.Margin = new Padding(3, 2, 3, 2);
             button16.Name = "button16";
-            button16.Size = new Size(63, 47);
+            button16.Size = new Size(55, 35);
             button16.TabIndex = 21;
             button16.UseVisualStyleBackColor = false;
             button16.Click += button16_Click;
-            // 
-            // panel1
-            // 
-            panel1.BackColor = Color.LightSteelBlue;
-            panel1.Controls.Add(lbl_result);
-            panel1.Controls.Add(tboxscreen);
-            panel1.Controls.Add(label1);
-            panel1.Location = new Point(77, 185);
-            panel1.Name = "panel1";
-            panel1.Size = new Size(194, 146);
-            panel1.TabIndex = 22;
-            panel1.Paint += panel1_Paint;
             // 
             // lbl_result
             // 
             lbl_result.BackColor = SystemColors.Window;
             lbl_result.BorderStyle = BorderStyle.Fixed3D;
             lbl_result.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            lbl_result.Location = new Point(-8, 92);
+            lbl_result.Location = new Point(82, 267);
             lbl_result.Name = "lbl_result";
             lbl_result.RightToLeft = RightToLeft.Yes;
-            lbl_result.Size = new Size(207, 54);
+            lbl_result.Size = new Size(188, 55);
             lbl_result.TabIndex = 2;
             // 
             // tboxscreen
             // 
             tboxscreen.Anchor = AnchorStyles.Top | AnchorStyles.Bottom;
             tboxscreen.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point);
-            tboxscreen.Location = new Point(-8, 39);
+            tboxscreen.Location = new Point(82, 209);
+            tboxscreen.Margin = new Padding(3, 2, 3, 2);
             tboxscreen.Name = "tboxscreen";
             tboxscreen.RightToLeft = RightToLeft.Yes;
-            tboxscreen.Size = new Size(207, 54);
+            tboxscreen.Size = new Size(188, 56);
             tboxscreen.TabIndex = 1;
             tboxscreen.Text = "";
             tboxscreen.TextChanged += tboxscreen_TextChanged;
@@ -285,9 +288,9 @@
             // 
             label1.BackColor = Color.Crimson;
             label1.Font = new Font("Showcard Gothic", 12F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(0, 0);
+            label1.Location = new Point(82, 180);
             label1.Name = "label1";
-            label1.Size = new Size(199, 36);
+            label1.Size = new Size(183, 27);
             label1.TabIndex = 0;
             label1.Text = "delete";
             label1.TextAlign = ContentAlignment.MiddleCenter;
@@ -295,12 +298,14 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = Properties.Resources.istockphoto_1160973722_1024x10241;
             BackgroundImageLayout = ImageLayout.Center;
-            ClientSize = new Size(386, 632);
-            Controls.Add(panel1);
+            ClientSize = new Size(586, 616);
+            Controls.Add(tboxscreen);
+            Controls.Add(lbl_result);
+            Controls.Add(label1);
             Controls.Add(button16);
             Controls.Add(button15);
             Controls.Add(button14);
@@ -318,10 +323,12 @@
             Controls.Add(button2);
             Controls.Add(button1);
             FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Margin = new Padding(3, 2, 3, 2);
+            MaximizeBox = false;
+            MdiChildrenMinimizedAnchorBottom = false;
             Name = "Form1";
             Text = "Form1";
             Load += Form1_Load;
-            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -343,7 +350,6 @@
         private Button button14;
         private Button button15;
         private Button button16;
-        private Panel panel1;
         private Label label1;
         private RichTextBox tboxscreen;
         private Label lbl_result;
